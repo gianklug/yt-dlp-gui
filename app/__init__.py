85fb3ebc9c05badebbc103cf07583a3f0c779c83
Gian Klug <gian.klug@ict-scouts.ch>
2022-09-03 23:03:07 +0200

    Add DB View, parse metadata correctly

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.shortcuts import redirect
 from flask import Flask, render_template, request, redirect
 from yt_dlp import YoutubeDL
@@ -9,29 +10,72 @@ from pprint import pprint
 
 
 # Initialize DB
-def init_db(con):
+def init_db():
+    con = sqlite3.connect("data.db")
     c = con.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS yt (id INTEGER PRIMARY KEY AUTOINCREMENT, url VARCHAR(256), filename VARCHAR(256), playlist VARCHAR(256), status INTEGER)")
     con.commit()
+    con.close()
 
+# Add Song to DB
+
+
+def add_song_to_db(url, playlist):
+    con = sqlite3.connect("data.db")
+    c = con.cursor()
+    c.execute("INSERT INTO yt (url, playlist, status) VALUES (?, ?, ?)",
+              (url, playlist, 2))
+    con.commit()
+    c.execute("SELECT last_insert_rowid()")
+    id = c.fetchone()
+    con.close()
+    return id[0]
+# Query Songs from DB
+
+
+def get_songs_from_db(limit=10):
+    con = sqlite3.connect("data.db")
+    c = con.cursor()
+    c.execute("SELECT * FROM yt ORDER BY id DESC LIMIT ?", (str(limit), ))
+    rows = c.fetchall()
+    con.close()
+    return rows
+
+# Update Song in DB
+
+
+def update_song_in_db(id, status, filename=""):
+    con = sqlite3.connect("data.db")
+    c = con.cursor()
+    c.execute("UPDATE yt SET filename=?, status=? WHERE id=?",
+              (filename, status, id))
+    con.commit()
+    con.close()
 
 # Get playlist list from azuracast api
+
+
 def get_playlists():
     headers = {"Authorization": f"Bearer {config['api_key']}"}
     data = requests.get(
         f"{config['azuracast_host']}/api/station/{config['station_id']}/playlists", headers=headers)
 
     playlists = [{"id": key["id"], "name": key["name"]} for key in data.json()]
-    #playlists = ["Rickrolling", "Rick Astley", "Give UP!"]
     return playlists
+
+
+def get_playlist_name(id):
+    headers = {"Authorization": f"Bearer {config['api_key']}"}
+    data = requests.get(
+        f"{config['azuracast_host']}/api/station/{config['station_id']}/playlist/{id}", headers=headers).json()
+    return data["name"]
 
 
 # Create the flask app
 app = Flask(__name__, static_url_path='/static')
 
 # Connect the sqlite DB
-con = sqlite3.connect("data.db")
-init_db(con)
+init_db()
 
 
 # Parse the config
@@ -44,9 +88,12 @@ config = parser["config"]
 def index():
     return render_template('index.html.j2', playlists=get_playlists())
 
+
 @app.route('/status')
 def status():
-    return render_template('status.html.j2')
+    songs = get_songs_from_db()
+    return render_template('status.html.j2', songs=songs)
+
 
 @app.route('/download')
 def download():
@@ -54,21 +101,37 @@ def download():
     if not url:
         return "Expected 1 parameter for 'url', got 0."
 
+    playlist = request.args.get('playlist', default=None, type=str)
+    if not url:
+        return "Expected 1 parameter for 'playlist', got 0."
+
+    # Get playlist human readable name
+    playlist_name = get_playlist_name(playlist)
+
+    # Add the song to the database
+    db_id = add_song_to_db(url, playlist_name)
+
     ydl_opts = {
         'format': 'bestaudio[ext=mp3]/best',
         'writethumbnail': True,
-        'outtmpl': '%(title)s.%(ext)s',
+        'outtmpl': './%(artist)s/%(title)s.%(ext)s',
         'postprocessors': [
             {'key': 'FFmpegExtractAudio',
              'preferredcodec': 'mp3',
              'preferredquality': '192'},
-            {'key': 'MetadataFromTitle',
-             'titleformat': '(?P<title>.+)\ \-\ (?P<artist>.+)'},
+            {'key': 'MetadataFromField',
+             'formats': {"title:%(artist)s - %(meta_title)s"}, },
             {'key': 'FFmpegMetadata'},
             {'key': 'EmbedThumbnail'}]}
 
     with YoutubeDL(ydl_opts) as ydl:
-        path = ydl.download(url)
-        print(path)
+        try:
+            download = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(download)
+
+            update_song_in_db(db_id, 0, filename)
+
+        except:
+            update_song_in_db(db_id, 1)
 
     return redirect("/")
